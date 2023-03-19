ae19d3f1228342a675b71509f77f9801f1499431
Daniel Galicia <danielaristeogaliciaruiz@gmail.com>
2023-03-19 09:21:12 -0600

    refactor:Initial commit, setting seeders

---
+++ database/seeders/20230208032826-users.js
@@ -0,0 +1,68 @@
+'use strict'
+const uuid = require('uuid')
+const { Op } = require('sequelize')
+const { hashPassword } = require('../../libs/bcrypt')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()
+
+    const usersSeeds = [
+      {
+        id: uuid.v4(),
+        first_name: 'Daniel',
+        last_name: 'Galicia',
+        email: 'daniel@academlo.com',
+        username: 'daniel@academlo.com',
+        password: hashPassword('12345678910'),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: uuid.v4(),
+        first_name: 'Mateo',
+        last_name: 'Castaño',
+        email: 'mateo@academlo.com',
+        username: 'mateo@academlo.com',
+        password: hashPassword('12345678910'),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]
+
+    try {
+      await queryInterface.bulkInsert('users', usersSeeds, { transaction })
+
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()
+
+    const userNames = [
+      'example@academlo.com',
+    ]
+
+    try {
+      await queryInterface.bulkDelete(
+        'users',
+        {
+          username: {
+            [Op.or]: userNames,
+          },
+        },
+        { transaction }
+      )
+
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  },
+}
